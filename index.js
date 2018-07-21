f318de20a87afdaad5422f918ea6343d5ef2854b
tasos <tasoskakour@gmail.com>
2018-07-21 16:34:46 +0300

    Refactored index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const LATEST_PROBLEM = 1; // This must be updated every time a new problem is solved.
+const LATEST_PROBLEM = 2; // This must be updated every time a new problem is solved.
 
-if (process.argv && process.argv[2]) {
-    require(`./solutions/${process.argv[2]}`)();
-} else {
-    for (let i = 1; i <= LATEST_PROBLEM; i++) {
-        try {
-            let sol = require(`./solutions/${i}`)();
-            console.log(`~ Solution for problem ${i} is: ${sol}`);
-        } catch (error) {
-            if (error.message !== `Cannot find module './solutions/${i}'`) {
-                console.log(error);
-                process.exit(1);
-            } else {
-                console.log(`-- Mising solution number ${i} --`);
+const main = () => {
+    if (process.argv && process.argv[2]) {
+        resultOutput(process.argv[2], require(`./solutions/${process.argv[2]}`)());
+    } else {
+        for (let i = 1; i <= LATEST_PROBLEM; i++) {
+            try {
+                resultOutput(i, require(`./solutions/${i}`)());
+            } catch (error) {
+                if (error.message !== `Cannot find module './solutions/${i}'`) {
+                    console.log(error);
+                    process.exit(1);
+                } else {
+                    console.log(`-- Mising solution number ${i} --`);
+                }
             }
         }
     }
 }
+
+const resultOutput = (i, sol) => {
+    console.log(`~ Solution for problem ${i} is: ${sol}`);
+}
+
+main();
