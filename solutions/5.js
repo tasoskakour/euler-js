d266d86d8dcee82c92308f5672188fe2de1c8d95
tasos <tasoskakour@gmail.com>
2018-07-21 19:33:08 +0300

    Solution 5

---
+++ solutions/5.js
@@ -0,0 +1,8 @@
+module.exports = () => {
+    const dividers = [...Array(19).keys()].map(v => v + 2);
+    let num = 40;
+    while (true) {
+        if (dividers.every(d => num % d === 0)) { return num; }
+        num += 20;
+    }
+}
