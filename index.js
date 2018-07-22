b3f2ccc52a43936e31e4307262ad17be775c3a7e
tasos <tasoskakour@gmail.com>
2018-07-22 10:16:26 +0300

    Solution 8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // IMPORTANT, Do not hurt the next line. It's configured from solved.sh 
-const LATEST_PROBLEM = 7;
+const LATEST_PROBLEM = 8;
 
 const main = () => {
     if (process.argv && process.argv[2]) {
