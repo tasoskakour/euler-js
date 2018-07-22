06ca85ce96441cd96d124e41d9940bdf8a2da946
tasos <tasoskakour@gmail.com>
2018-07-22 13:58:54 +0300

    Solution 14

---
+++ solutions/014.js
@@ -0,0 +1,23 @@
+module.exports = () => {
+    let maxChain = 1; maxStartNum = 3;
+    for (n = 3; n < 1e6; n++) {
+        let chain = 1;
+        let i = n;
+        while (true) {
+            if (i % 2 === 0) {
+                i = i / 2;
+            } else {
+                i = 3 * i + 1;
+            }
+            chain++;
+            if (i === 1) {
+                break;
+            }
+        }
+        if (chain > maxChain) {
+            maxChain = chain;
+            maxStartNum = n;
+        }
+    }
+    return maxStartNum;
+}
