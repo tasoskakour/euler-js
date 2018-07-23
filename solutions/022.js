13da37ae2dc9054f4504d47783bfba91cc8ae781
tasos <tasoskakour@gmail.com>
2018-07-23 21:41:27 +0300

    Solution 22

---
+++ solutions/022.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = () => {
+    return fs.readFileSync('utilities/problems-data/022/names.txt', 'utf8').replace(/"/g, '').split(',').sort()
+        .map((n, i) => n.split('').map(c => c.charCodeAt(0) - 64).reduce((a, b) => a + b, 0) * (i + 1))
+        .reduce((a, b) => a + b, 0)
+}
