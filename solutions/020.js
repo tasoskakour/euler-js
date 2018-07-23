8f1e704aef0ab546b13ee7f7f677638f4a604df9
tasos <tasoskakour@gmail.com>
2018-07-23 20:32:59 +0300

    Solution 20

---
+++ solutions/020.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+    let digits = [1];
+    for (let i = 2; i <= 100; i++) {
+        let carry = 0;
+        for (let d = digits.length - 1; d >= 0; d--) {
+            let mul = digits[d] * i + carry;
+            if (mul >= 10) {
+                carry = Math.floor(mul / 10);
+                mul -= 10 * carry;
+            } else {
+                carry = 0;
+            }
+            digits[d] = mul;
+        }
+        if (carry !== 0) {
+            digits.unshift(...String(carry).split('').map(v => Number(v)));
+        }
+    }
+    return digits.reduce((a, b) => a + b, 0);
+}
