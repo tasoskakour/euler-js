dafeec8421b478eb0ccfeb10f6fc1af4c6248d01
tasos <tasoskakour@gmail.com>
2018-07-24 21:48:34 +0300

    Solution 23

---
+++ solutions/023.js
@@ -0,0 +1,37 @@
+const getDivisors = require('../utilities/lib').getDivisors
+
+module.exports = () => {
+    let cache = { 12: true };
+    let sum = 0;
+    for (let n = 1; n <= 28123; n++) {
+        let isSumOfAbundant = false;
+        let i = 12;
+        while (i <= Math.floor(n / 2)) {
+            let isA = cache[i];
+            if (isA === undefined) {
+                isA = isAbundant(i);
+                cache[i] = isA;
+            }
+            if (isA) {
+                let is2A = cache[n - i];
+                if (is2A === undefined) {
+                    is2A = isAbundant(n - i);
+                    cache[n - i] = is2A;
+                }
+                if (is2A) {
+                    isSumOfAbundant = true;
+                    break;
+                }
+            }
+            i++;
+        }
+        if (!isSumOfAbundant) {
+            sum += n;
+        }
+    }
+    return sum;
+}
+
+const isAbundant = (num) => {
+    return getDivisors(num, true).reduce((a, b) => a + b, 0) > num;
+}
