f3f29d85779cef069b2187d09b1717d7f39cde4a
tasos <tasoskakour@gmail.com>
2018-07-26 20:21:55 +0300

    Solution 31

---
+++ solutions/031.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+    const max = 200;
+    let count = 0;
+    for (let a = max; a >= 0; a -= 200) {
+        for (let b = a; b >= 0; b -= 100) {
+            for (let c = b; c >= 0; c -= 50) {
+                for (let d = c; d >= 0; d -= 20) {
+                    for (let e = d; e >= 0; e -= 10) {
+                        for (let f = e; f >= 0; f -= 5) {
+                            for (let g = f; g >= 0; g -= 2) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return count;
+}
