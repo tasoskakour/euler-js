ac8d141c07eb858101c71ccdee5078b1ca9a2c77
tasos <tasoskakour@gmail.com>
2018-07-26 18:41:19 +0300

    Solution 30

---
+++ solutions/030.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+    let N = Array(7).fill(Math.pow(9, 5)).reduce((a, b) => a + b, 0); // 7*9^5 << 9999999
+    let sum = 0;
+    for (n = 32; n <= N; n++) {
+        if (String(n).split('').reduce((a, b) => a + Math.pow(Number(b), 5), 0) === n) {
+            console.log(n);
+            sum += n;
+        }
+    }
+    return sum;
+}
