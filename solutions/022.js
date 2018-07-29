1614a37d78f848387d64b83564ece7c71abb56cc
tasos <tasoskakour@gmail.com>
2018-07-29 11:08:35 +0300

    Folder Restructruring

--- solutions/022.js
+++ solutions/022.js
@@ -3,10 +3,9 @@
     https://projecteuler.net/problem=22
 */
 const fs = require('fs');
-const path = require('path');
 
 module.exports = () => {
-    return fs.readFileSync('utilities/problems-data/022/names.txt', 'utf8').replace(/"/g, '').split(',').sort()
+    return fs.readFileSync('utilities/problems-data/p022_names.txt', 'utf8').replace(/"/g, '').split(',').sort()
         .map((n, i) => n.split('').map(c => c.charCodeAt(0) - 64).reduce((a, b) => a + b, 0) * (i + 1))
         .reduce((a, b) => a + b, 0)
 }
